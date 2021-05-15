e66b674aeb21bf156cfbfbedf3de097f574d057c
ericpkatz <ericpkatz@gmail.com>
2021-05-15 09:34:50 -0400

    separate out db

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const pg = require('pg');
 const express = require('express');
+
+const db = require('./db');
+const { client, syncAndSeed, getBrands, createBrand } = db;
 
 const app = express();
 
@@ -30,57 +32,6 @@ app.get('/', async(req, res, next)=> {
     next(ex);
   }
 });
-
-const client = new pg.Client('postgres://localhost/sneaker_world_db');
-
-const createBrand = async(name)=> {
-  return (await client.query(`
-  INSERT INTO brands(name) values($1) returning *
-  `, [name])).rows[0];
-};
-
-const getBrands = async()=> {
-  return (await client.query('SELECT * from brands')).rows;
-};
-
-const getBrand = async(id)=> {
-  const response = await client.query('SELECT * FROM brands WHERE id = $1', [id]);
-  return response.rows[0];
-};
-
-const syncAndSeed = async()=> {
-  const SQL = `
-  DROP TABLE IF EXISTS ownership;
-DROP TABLE IF EXISTS sneakers;
-DROP TABLE IF EXISTS brands;
-DROP TABLE IF EXISTS collectors;
-
-CREATE TABLE brands(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
-);
-
-CREATE TABLE sneakers(
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(100),
-  brand_id INTEGER REFERENCES brands(id)
-);
-
-CREATE TABLE collectors(
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(100)
-);
-
-CREATE TABLE ownership(
-  id INTEGER PRIMARY KEY,
-  size INTEGER,
-  condition VARCHAR(10),
-  sneaker_id INTEGER REFERENCES sneakers(id),
-  collector_id INTEGER REFERENCES collectors(id)
-);
-  `;
-  await client.query(SQL);
-};
 
 const start = async()=> {
   try {
